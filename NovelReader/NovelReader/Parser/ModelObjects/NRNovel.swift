17222086416f769559f96d287aa83176b7ac6098
PraveenP <ppraveentr@gmail.com>
2018-04-27 22:25:08 +0530

    Updated recent novelList service binding. Fixed Runtime error for FTServiceStack.setupServiceRequest where internal getter is misused.

--- NovelReader/NovelReader/Parser/ModelObjects/NRNovel.swift
+++ NovelReader/NovelReader/Parser/ModelObjects/NRNovel.swift
@@ -2,30 +2,39 @@ final class NRNovel: FTModelData {
 
     var identifer: String? = nil
     var lastChapter: String? = nil
-    var imageURL: String? = nil
     var chapterList: [NRNovelChapter]? = nil
-    var contentDescription: String? = nil
-    var views: String? = nil
-    var keyworkds: [String]? = nil
     var searchString: String? = nil
-    var title: String? = nil
+    var rating: String? = nil
     var author: String? = nil
     var lastUpdated: String? = nil
+    var novelURL: String? = nil
+    var imageURL: String? = nil
+    var keyworkds: [String]? = nil
+    var contentDescription: String? = nil
+    var artist: String? = nil
+    var status: String? = nil
+    var views: String? = nil
+    var title: String? = nil
+    var novelType: String? = nil
 
     /* Coding Keys */
     enum CodingKeys: String, CodingKey  {
         case identifer = "id"
         case lastChapter = "lastchapter"
-        case imageURL = "image"
         case chapterList
-        case contentDescription = "description"
-        case views = "view"
-        case keyworkds
         case searchString = "nameunsigned"
-        case title = "name"
+        case rating
         case author
         case lastUpdated = "updatetime"
+        case novelURL = "url"
+        case imageURL = "image"
+        case keyworkds = "genres"
+        case contentDescription = "summary"
+        case artist
+        case status
+        case views
+        case title = "name"
+        case novelType = "type"
 
     }
 }
-
